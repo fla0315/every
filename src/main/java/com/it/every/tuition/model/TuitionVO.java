908ae1a08222bd8c594554d9b674ae2b4bafebf9
Yeong-rim Kim <74998088+fla0315@users.noreply.github.com>
2021-07-16 15:15:45 +0900

    1

--- src/main/java/com/it/every/tuition/model/TuitionVO.java
+++ src/main/java/com/it/every/tuition/model/TuitionVO.java
@@ -13,10 +13,11 @@ public class TuitionVO {
 
 	private int no; /* 등록금번호 */
 	private String stu_no; /* 학번 */
-	private String semester; /* 학기 */
-	private int tuition;/* 금액 */
-	private String deposit_state; /* 납부여부 */
-	private Timestamp deposit_date; /* 납부일 */
+	private String semester; /* 학 기 */
+	private int tuition;/* 금 액 */
+	private String deposit_state; /* 납 부여 부 */
+	private Timestamp deposit_date; /* 납 부일  */
+	
 	
 	
 }
