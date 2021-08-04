0ca97d1ddeb392f9f7d0ff8c6daf32a94b17dc69
Yeong-rim Kim <74998088+fla0315@users.noreply.github.com>
2021-08-04 10:09:59 +0900

    Update DistributeAssignVO.java

--- src/main/java/com/it/every/distributeAssign/model/DistributeAssignVO.java
+++ src/main/java/com/it/every/distributeAssign/model/DistributeAssignVO.java
@@ -9,9 +9,12 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class DistributeAssignVO {
-	private int assignNo;			//과제번호
-	private String openSubCode;		//개설교과목코드
-	private String assignName;		//과제명
-	private Timestamp regDate;		//등록일
+public class DistributeAssignVo {
+	 
+	private int assignNo; //과제번호
+	private String openSubCode; //과제교과과목코드
+	private String assignName; //과제이름
+	private Timestamp regDate; //제출일
+	private String studentId;
+	
 }
