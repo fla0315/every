2fddaaa9fee7d1c06b968e4cb2a2c16bec598a77
jky308 <rabby_j@naver.com>
2021-08-10 16:11:11 +0900

    1
    1

--- src/main/java/com/it/every/post/model/PostServiceImpl.java
+++ src/main/java/com/it/every/post/model/PostServiceImpl.java
@@ -17,104 +17,117 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
-	private final PostDAO postDao;
+   private final PostDAO postDao;
 
-	@Override
-	public List<PostVO> postByOpenSubCode(int bdCode) {
-		return postDao.postByOpenSubCode(bdCode);
-	}
+   @Override
+   public List<PostVO> postByOpenSubCode(int bdCode) {
+      return postDao.postByOpenSubCode(bdCode);
+   }
 
-	@Override
-	public int insertNoitce(PostVO vo) {
-		return postDao.insertNoitce(vo);
-	}
+   @Override
+   public int insertNoitce(PostVO vo) {
+      return postDao.insertNoitce(vo);
+   }
 
-	@Override
-	public Map<String, Object> postByPostNo(int postNo) {
-		return postDao.postByPostNo(postNo);
-	}
+   @Override
+   public Map<String, Object> postByPostNo(int postNo) {
+      return postDao.postByPostNo(postNo);
+   }
 
-	@Override
-	public String contentsByPostNo(int postNo) {
-		return postDao.contentsByPostNo(postNo);
-	}
+   @Override
+   public String contentsByPostNo(int postNo) {
+      return postDao.contentsByPostNo(postNo);
+   }
 
-	@Override
-	public int noticeReadCount(int postNo) {
-		return postDao.noticeReadCount(postNo);
-	}
+   @Override
+   public int noticeReadCount(int postNo) {
+      return postDao.noticeReadCount(postNo);
+   }
 
-	@Override
-	public int deletePostByPostNo(int postNo) {
-		return postDao.deletePostByPostNo(postNo);
-	}
+   @Override
+   public int deletePostByPostNo(int postNo) {
+      return postDao.deletePostByPostNo(postNo);
+   }
 
-	@Override
-	public int editNotice(PostVO vo) {
-		return postDao.editNotice(vo);
-	}
-	
-		
-	public Map<String,  Object> selectByJunggoPostNo(int postNo) {
-		return postDao.selectByJunggoPostNo(postNo);
-	}
+   @Override
+   public int editNotice(PostVO vo) {
+      return postDao.editNotice(vo);
+   }
+   
+      
+   public Map<String,  Object> selectByJunggoPostNo(int postNo) {
+      return postDao.selectByJunggoPostNo(postNo);
+   }
 
-	@Override
-	public int updateByJunggoPostNo(int postNo) {
-		return postDao.updateByJunggoPostNo(postNo);
-	}
+   @Override
+   public int updateByJunggoPostNo(int postNo) {
+      return postDao.updateByJunggoPostNo(postNo);
+   }
 
-	@Override
-	public int insertJunggo(PostVO vo) {
-		return postDao.insertJunggo(vo);
-	}
+   @Override
+   public int insertJunggo(PostVO vo) {
+      return postDao.insertJunggo(vo);
+   }
 
-	@Override
-	public int insertJunggoFile(BoardFileVO vo) {
-		return postDao.insertJunggoFile(vo);
-	}
+   @Override
+   public int insertJunggoFile(BoardFileVO vo) {
+      return postDao.insertJunggoFile(vo);
+   }
 
-	@Override
-	public int junggoBdCode() {
-		return postDao.junggoBdCode();
-	}
+   @Override
+   public int junggoBdCode() {
+      return postDao.junggoBdCode();
+   }
 
-	@Override
-	public List<Map<String, Object>> postBybdCode(int bdCode) {
-		return postDao.postBybdCode(bdCode);
-	}
-
-
-	public List<Map<String, Object>> selectByNoticeStudent(RegistrationSearchVO regiVo) {
-		return postDao.selectByNoticeStudent(regiVo);
-	}
-
-	@Override
-	public List<Map<String, Object>> noticeByProfNo(String profNo) {
-		return postDao.noticeByProfNo(profNo);
-	}
-
-	@Override
-	public int freewrite(PostVO VO) {
-		return postDao.freewrite(VO);
-	}
-
-	@Override
-	public List<PostVO> freepost() {
-		return postDao.freepost();
-	}
-
-	@Override
-	public List<PostVO> detail(int no) {
-		return postDao.detail(no);
-	}
-
-	@Override
-	public int deletepost(int a) {
-		return postDao.deletepost(a);
-
-	}
+   @Override
+   public List<Map<String, Object>> postBybdCode(int bdCode) {
+      return postDao.postBybdCode(bdCode);
+   }
 
 
+   public List<Map<String, Object>> selectByNoticeStudent(RegistrationSearchVO regiVo) {
+      return postDao.selectByNoticeStudent(regiVo);
+   }
+
+   @Override
+   public List<Map<String, Object>> noticeByProfNo(String profNo) {
+      return postDao.noticeByProfNo(profNo);
+   }
+
+   @Override
+   public int freewrite(PostVO VO) {
+      return postDao.freewrite(VO);
+   }
+
+   @Override
+   public List<PostVO> freepost() {
+      return postDao.freepost();
+   }
+
+   @Override
+   public List<PostVO> detail(int no) {
+      return postDao.detail(no);
+   }
+
+   @Override
+   public int deletepost(int a) {
+      return postDao.deletepost(a);
+
+   }
+
+   @Override
+   public int updatecount(int no) {
+      return postDao.updatecount(no);
+   }
+
+   @Override
+   public List<PostVO> mylist(String no) {
+      return postDao.mylist(no);
+   }
+
+   @Override
+   public int updatepost(PostVO vo) {
+      return postDao.updatepost(vo);
+   }
 
 }
